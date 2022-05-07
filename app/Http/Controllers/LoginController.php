df87480a9b53a9afc79bbdab33b237a3c16d8b01
Bean Red Army <69241942+linhdan2812@users.noreply.github.com>
2022-05-07 11:19:52 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -97,32 +97,16 @@ class LoginController extends Controller
 
     //Google callback
     public function handleGoogleCallback(){
-        $google = Socialite::driver('google')->stateless()->user();
+        $user = Socialite::driver('google')->stateless()->user();
+        $this->createOrUpdateUser($user,'google');
         $mail = substr($user->email,-11);
         // if(substr($user->email,-11)=="@fpt.edu.vn" && Auth::user()->role === 'ADM'){
         //     return redirect()->route('admin.dashboard');}
         // tạm thời tài khoản giảng viên là @fpt.edu.vn, còn sinh viên là @gmail.com
         // Str::endsWith($user->email, '@fpt.edu.vn')
         if(Str::endsWith($user->email, '@gmail.com')=='true'){
-            $user= User::updateOrCreate([
-                'user'=>$user->id,
-            ],[
-                'name' => $google->name,
-                'email' => $google->email,
-                'github_token' => $google->token,
-                'github_refresh_token' => $google->refreshToken,
-                'role' => 'USR',
-            ]); Auth::login($user);
             return redirect()->route('welcome');
         }elseif(Str::endsWith($user->email, '@fpt.edu.vn')=='true' && Auth::user()->role === 'TT'){
-            $user= User::updateOrCreate([
-                'user'=>$user->id,
-            ],[
-                'name' => $google->name,
-                'email' => $google->email,
-                'github_token' => $google->provider,
-                'role' => 'TT',
-            ]); Auth::login($user);
             return redirect()->route('tutor.dashboard');
         }else{
             return redirect()->route('login')->with('msg1','Tài khoản Google không hợp lệ');
